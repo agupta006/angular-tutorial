ecac4aa50aacafb7fb2dbfeacffc12af0184936a
ANKIT\Ankit Gupta <agupta255@sapient.com>
2017-03-07 22:38:31 +0530

    experimenting with built in pipes

--- src/app/views/pipes/available-pipes/available-pipes.component.ts
+++ src/app/views/pipes/available-pipes/available-pipes.component.ts
@@ -10,6 +10,17 @@ export class AvailablePipesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  
+   
   }
+    name = "birthday";
+    birthday = new Date(1993, 1, 6); // mm starts with 0
+
+    toggle_flag = true;
+    toggle (){
+      this.toggle_flag = !this.toggle_flag;
+    }
+
+    get format(){return this.toggle_flag ? 'shortDate' : 'fullDate';  }
 
 }
